6d60a4357499460457dcc97f04f3af9c165c800a
eireland <eireland@concord.org>
2018-12-20 15:52:56 -0800

    Initial commit of element locators

---
+++ cypress/plugins/elements/Footer.js
@@ -0,0 +1,40 @@
+class Footer {
+    getFooter(){
+        return cy.get('.bottom-bar')
+    }
+    getLeftPolarityButton(){
+        return cy.get('.bottom-bar.unrolled > .row > .polarity-button')
+    }
+    getLeftStrengthSlider(){
+        return cy.get('.bottom-bar.unrolled > .row > .strength-control-panel > div > .slider')
+    }
+    getRightPolarityButton(){
+        return cy.get('.bottom-bar.unrolled > .row > .row> .polarity-button')
+    }
+    getRightStrengthSlider(){
+        return cy.get('.bottom-bar.unrolled > .row > .row > .strength-control-panel > div > .slider')
+    }
+    getCompass(){
+        return cy.get('[data-test=compass]');
+    }
+    turnOnFieldLines(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Field Lines').click();
+    }
+    turnOffFieldLines(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Field Lines').click();
+    }
+    turnOnCloud(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Cloud').click();
+    }
+    turnOffCloud(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Cloud').click();
+    }
+    turnOnPointers(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Pointers').click();
+    }
+    turnOffPointers(){
+        cy.get('.mag-field-control-panel>.checkbox-container>.container').contains('Pointers').click();
+    }
+}
+
+export default Footer;
