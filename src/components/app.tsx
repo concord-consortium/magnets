b342762fb38434ec20ff8cbe4c78174ed407d09c
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-01-02 22:13:49 -0800

    Scale elements when window is resized

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,19 +9,55 @@ import { MainContentComponent } from "./main-content";
 import { BottomBarComponent } from "./bottom-bar";
 
 interface IProps extends IBaseProps {}
-interface IState {}
+interface IState {
+  width: number;
+  height: number;
+}
+const kAppWidth = 960;
+const kAppHeight = 850; // todo: should be 618
 
 @inject("stores")
 @observer
 export class AppComponent extends BaseComponent<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  public componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  public updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
 
   public render() {
-    const {ui} = this.stores;
+    const widthRatio = this.state.width / kAppWidth;
+    const heightRatio = this.state.height / kAppHeight;
+    let scalePercent = widthRatio > heightRatio ? heightRatio : widthRatio;
+    if (scalePercent > 1) {
+      scalePercent = 1;
+    }
+    const styleVal = "scale(" + scalePercent + ")";
+    const styles = {
+      transform: styleVal
+    };
     return (
-      <div className="app-container">
+      <div style={styles} className="app-container">
         <TopBarComponent />
         <MainContentComponent />
         <BottomBarComponent />
+        <div className="cover"/>
       </div>
     );
   }
