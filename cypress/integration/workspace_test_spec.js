aef4110d64cd4f817c56cc65e35f9a905fb8214f
eireland <eireland@concord.org>
2019-01-03 16:41:14 -0800

    Updated header tests with new UI and selectors

--- cypress/integration/workspace_test_spec.js
+++ cypress/integration/workspace_test_spec.js
@@ -17,60 +17,58 @@ context('Test whole workspace layout', ()=>{
 
         });
         it('will verify top bar is not visible when select magnet button is clicked and top bar is already showing', ()=>{
-            // header.selectMagnetLeft();
-            // header.getTopBar().should('not.be.visible');
+            header.selectMagnetLeft();
+            header.getTopBar().should('not.be.visible');
         });
         it('will verify top bar is shown when left select magnet button is clicked', ()=>{
             header.selectMagnetLeft();
             header.getTopBar().should('be.visible');
         });
         it('will verify movable magnet cannot be added when fixed magnet is not present', ()=>{
-            header.selectBarMagRight();
-            //nothing should happen
-            header.selectCoilMagRight();
-            //nothing should happen
+            header.getBarMagRight().parent().should('have.class', 'disabled');
+            header.getCoilMagRight().parent().should('have.class', 'disabled');
         });
         it('will verify footer is not visible when right magnet is selected', ()=>{
-            footer.getFooter().should('not.have.class', 'unrolled');
+            footer.getFooter().should('not.be.visible');
         });
-        it('will verify that correct magnet is shown when bar magnet is selected from the left', ()=>{
-            header.selectMagnetLeft();
+        it('will verify that correct magnet is shown when bar magnet is selected from the left', ()=> {
+            // header.selectMagnetLeft(); Header is already showing
             header.selectBarMagLeft();
-            header.getSelectedMagLeft().children('img').should('contain','magnet-bar.png');
+            header.getSelectedMagLeft().siblings('.nav-magnet.left.bar').should('be.visible');
         });
         it('will verify that correct magnet is shown when coil magnet is selected from the left', ()=>{
-            header.selectMagnetLeft();
+            // header.selectMagnetLeft();
             header.selectCoilMagLeft();
-            header.getSelectedMagLeft().children('img').should('contain','magnet-coil.png');
+            header.getSelectedMagLeft().siblings('.nav-magnet.left.coil').should('be.visible');
         });
         it('will verify footer is visible when left magnet is selected', ()=>{
-            footer.getFooter().should('have.class', 'unrolled');
+            footer.getFooter().should('be.visible');
         });
         it('will verify that different combinations of left and right magnets come up correctly', ()=>{
             header.selectBarMagLeft();
-            header.getSelectedMagLeft().children('img').should('contain','magnet-bar.png');
-            //mainapp shows bar magnet
+            header.getSelectedMagLeft().siblings('.nav-magnet.left.bar').should('be.visible');
+            //mainapp shows bar magnet - cypress take a screenshot?
             //mainapp should not be able to move the magnet
             header.selectBarMagRight();
-            header.getSelectedMagRight().children('img').should('contain','magnet-bar.png');
+            header.getSelectedMagRight().siblings('.nav-magnet.right.bar').should('be.visible');
             //mainapp shows two bar magnets
             //mainapp should have one bar that cannot be moved, and another that can be moved
             header.selectCoilMagLeft();
-            header.getSelectedMagLeft().children('img').should('contain','magnet-coil.png');
+            header.getSelectedMagLeft().siblings('.nav-magnet.left.coil').should('be.visible');
             //mainapp shows a coil magnet and a bar magnet
             //mainapp should have one coil that cannot be moved, and a bar that can be moved
             header.selectCoilMagRight();
-            header.getSelectedMagRight().children('img').should('contain','magnet-coil.png');
+            header.getSelectedMagRight().siblings('.nav-magnet.right.coil').should('be.visible');
             //mainapp shows two coil magnets
             //mainapp should have one coil that cannot be moved, and another that can be moved
             header.selectBarMagLeft();
-            header.getSelectedMagLeft().children('img').should('contain','magnet-bar.png');
+            header.getSelectedMagLeft().siblings('.nav-magnet.left.bar').should('be.visible');
             //mainapp shows a bar and a coil
             //mainapp should have one coil that cannot be moved, and the bar that can be moved
         });
         it('will verify only right magnet is removed when both left and right magnets are present', ()=>{
             header.removeMagRight();
-            header.getUnselectMagnetRight().should('be.visible');
+            header.getSelectAMagnetRight().should('be.visible');
             //mainspace should have a fixed bar magnet
         });
 
