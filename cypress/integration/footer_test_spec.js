18b36d8345aec9c2ac257ac5adf0f555c3e47a15
eireland <eireland@concord.org>
2019-01-09 15:38:29 -0800

    Added tests for showing mag field reps

--- cypress/integration/footer_test_spec.js
+++ cypress/integration/footer_test_spec.js
@@ -23,15 +23,59 @@ context('Test footer area', ()=>{
             footer.changeCenterPolarity();
             footer.getCenterBarPolarityToggle().should('have.text','N-S')
         });
-        it('will turn on mag field representations', ()=>{ //Need to turn field representations first so that changing field strength is more visible
-
+        it('will turn on mag field representations separately', ()=>{ //Need to turn field representations first so that changing field strength is more visible
+            footer.turnOnFieldLines();
+            footer.getMagneticFieldFieldLinesToggle().should('contain','ON');
+            //TODO: take snapshot
+            footer.turnOffFieldLines();
+            footer.getMagneticFieldFieldLinesToggle().should('contain','OFF');
+            //TODO: take snapshot
+            footer.turnOnCloud();
+            footer.getMagneticFieldCloudToggle().should('contain','ON');
+            //TODO: take snapshot
+            footer.turnOffCloud();
+            footer.getMagneticFieldCloudToggle().should('contain','OFF');
+            //TODO: take snapshot
+            footer.turnOnPointers();
+            footer.getMagneticFieldPointersToggle().should('contain','ON');
+            //TODO: take snapshot
+            footer.turnOffPointers();
+            footer.getMagneticFieldPointersToggle().should('contain','OFF');
+            //TODO: take snapshot
+        });
+        it('will turn on mag field representations in combinations',()=>{
+            footer.turnOnFieldLines();
+            footer.getMagneticFieldFieldLinesToggle().should('contain','ON');
+            footer.turnOnCloud();
+            footer.getMagneticFieldCloudToggle().should('contain','ON');
+            //TODO:take snapshot
+            footer.turnOffCloud();
+            footer.getMagneticFieldCloudToggle().should('contain','OFF');
+            //TODO: take snapshot
+            footer.turnOnPointers();
+            footer.getMagneticFieldPointersToggle().should('contain','ON');
+            //TODO: take snapshot
+            footer.turnOffFieldLines();
+            footer.getMagneticFieldFieldLinesToggle().should('contain','OFF');
+            //TODO: take snapshot
+            footer.turnOnCloud();
+            footer.getMagneticFieldCloudToggle().should('contain','ON');
+            //TODO: take snapshot
+            footer.turnOnFieldLines();
+            footer.getMagneticFieldFieldLinesToggle().should('contain','ON');
+            //TODO: take snapshot
         });
         it('will change field strength', ()=>{
             footer.changeFieldStrength(2);
+            //TODO: take snapshot
             footer.changeFieldStrength(3);
+            //TODO: take snapshot
             footer.changeFieldStrength(1);
+            //TODO: take snapshot
             footer.changeFieldStrength(3);
+            //TODO: take snapshot
             footer.changeFieldStrength(2);
+            //TODO: take snapshot
         })
 
     })
