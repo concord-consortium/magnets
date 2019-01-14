6004ee10c315edc1b127c0cffe813f33756d22bd
eireland <eireland@concord.org>
2019-01-14 12:21:13 -0800

    Added thresholds

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,2 +1,6 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
-addMatchImageSnapshotCommand();
+addMatchImageSnapshotCommand({
+    failureThreshold: 0.03, // threshold for entire image
+    failureThresholdType: 'percent', // percent of image or number of pixels
+    customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+});
