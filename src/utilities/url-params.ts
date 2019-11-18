49a3b0c98dc03ec2674831809202ed28d7943c19
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-11-18 11:41:56 -0800

    Add url param to show/hide second magnet option
    [#169712851]

--- src/utilities/url-params.ts
+++ src/utilities/url-params.ts
@@ -5,6 +5,8 @@ export interface QueryParams {
   appMode?: AppMode;
   // turn field representation control on/off
   fieldRepresentations?: string;
+  // the number of magnets, ?magnets=1 to only allow 1 magnet
+  magnets?: string;
 }
 
 const params = parse(location.search);
@@ -12,6 +14,7 @@ const params = parse(location.search);
 export const DefaultUrlParams: QueryParams = {
   appMode: "dev",
   fieldRepresentations: "false",
+  magnets: "2",
 };
 
 export const urlParams: QueryParams = params;
